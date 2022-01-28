b0000403cfda5197505da4764c12350297743f8c
Polycarp Anyanwu <polyanyanwu@gmail.com>
2022-01-28 16:12:04 +0000

    Add data model for tips and its js

---
+++ assets/js/model.js
@@ -0,0 +1,29 @@
+export const tipsData = [
+    {
+      id: 0, // uniquely identify this tip
+      used: false, // already displayed tip will be true and used to filter and ensure tips are not repeated
+      detail: `<p><h2>Relieving Neck Tension</h2>.
+      <br>Before you get started with any exercise, make sure you get rid of some of the tension that’s accumulated from hours of sitting at your desk.
+      These yoga moves for your neck and shoulders will allow you to avoid any headaches or neck pain later in the evening by taking the strain off your muscles.
+      Despite how simple these moves seem, they are very effective and involve no pain.</p> <p>
+      Just drop your chin, roll your head around, roll your shoulders both ways, and stretch your arms as in the video explainer above. Stop immediately if you feel any pain.
+      .</p>`,
+    },
+    {
+        id: 1, // uniquely identify this tip
+        used: false, // already displayed tip will be true and used to filter and ensure tips are not repeated
+        detail: `<p><h2>Wrist Stretches</h2>Particularly useful for people who use the computer a lot, these wrist stretches help you relieve and avoid wrist pain and other more serious conditions in the future.
+        </p> <p>For some quick ideas: Lift your palms, stretch your arms, press your palms into each other, shake your hands to get rid of any tension, or try some wrist curls using water bottles as weights.
+        .</p>`,
+      },
+      {
+        id: 2, // uniquely identify this tip
+        used: false, // already displayed tip will be true and used to filter and ensure tips are not repeated
+        detail: `<p><h2>High Knees</h2>Need some cardio? Stand up from your desk and try to get your knees as high as you possibly can.
+        This exercise is similar to running in place but you’re focusing on bringing your knees high. You can use your hands and tap the palms with your knees to make sure you’re doing the exercise correctly.        
+        </p> <p>Try going for three sets of 20 repetitions first. You can also use this quick exercise as a way to warm up before another workout too.
+        .</p>`,
+      },
+
+
+  ];
