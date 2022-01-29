05a260f4542a8330c9e2f22061f8ade619277f28
Polycarp Anyanwu <polyanyanwu@gmail.com>
2022-01-29 15:15:20 +0000

    Add read form data, add display lunchtime reminder at given time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
-import { tipsData } from "./model.js";
-import { TIPS_INTERVAL as tipInterval } from "./config.js";
+import {
+  tipsData
+} from "./model.js";
+import {
+  TIPS_INTERVAL as tipInterval
+} from "./config.js";
 const open = document.getElementById('open');
 const modal_container = document.getElementById('modal_container');
-const close = document.getElementById('close');
+// const close = document.getElementById('close');
 const tipsEl = document.getElementById("tips-main");
 const timerEl = document.getElementById("times");
 
@@ -11,21 +15,45 @@ const reminderHeadEl = document.querySelector("#reminder-header");
 const reminderAudioEl = document.querySelector("#reminder-audio");
 const reminderModalEl = document.querySelector("#modal_reminder");
 const closeReminder = document.querySelector('.close');
+const planForm = document.getElementById("plan-form");
+let name;
+let daystart;
+let lunchbreak;
+let dayend;
 
 let time = tipInterval;
 
 open.addEventListener('click', () => {
-    modal_container.classList.add('show');
+  modal_container.classList.add('show');
 });
 
-close.addEventListener('click', () => {
-    modal_container.classList.remove('show');
-});
+// close.addEventListener('click', () => {
+//     modal_container.classList.remove('show');
+// });
 
 closeReminder.addEventListener('click', () => {
   reminderModalEl.classList.remove('show');
 });
- 
+
+planForm.addEventListener('submit', (e) => {
+  // process the form content
+  e.preventDefault();
+  const data = e.target;
+  name = data['Name'].value;
+  daystart = data['daystart'].value;
+  lunchbreak = data['lunchbreak'].value;
+  dayend = data['dayend'].value;
+  modal_container.classList.remove('show');
+  console.log(name, daystart, lunchbreak, dayend);
+  console.log(typeof dayend);
+  getTime();
+});
+
+function processForm() {
+  console.log("processed form")
+  modal_container.classList.remove('show');
+
+}
 const startTimer = function () {
   const tick = function () {
     const hour = String(Math.trunc(time / 3600)).padStart(2, 0);
@@ -33,14 +61,16 @@ const startTimer = function () {
     // const min = String(Math.trunc(time / 60)).padStart(2, 0);
     const sec = String(time % 60).padStart(2, 0);
     // In each call, print the remaining time to UI, include hour if it is greater than 0
-    hour > 0
-      ? (timerEl.textContent = `${hour}:${min}:${sec}`)
-      : (timerEl.textContent = `${min}:${sec}`);
-
+    hour > 0 ?
+      (timerEl.textContent = `${hour}:${min}:${sec}`) :
+      (timerEl.textContent = `${min}:${sec}`);
+    if (getTime() == lunchbreak) {
+      displayReminder();
+    }
     // When 0 seconds, restart timer and display another tip
     if (time === 0) {
       time = tipInterval;
-      displayReminder();
+
       // displayTip();
     }
     // Decrease 1s
@@ -53,8 +83,9 @@ const startTimer = function () {
 };
 
 
-const init = function(){
+const init = function () {
   modal_container.classList.add('show');
+  // planForm.action = processForm();
   startTimer();
 }
 
@@ -93,3 +124,12 @@ const displayReminder = function () {
   reminderAudioEl.play();
   reminderModalEl.classList.add('show');
 };
+
+function getTime() {
+  const today = new Date();
+  let hour = today.getHours() + '';
+  let min = today.getMinutes() + '';
+  hour = hour.length == 1 ? '0' + hour : hour;
+  min = min.length == 1 ? '0' + min : min;
+  return hour + ":" + min;
+}
