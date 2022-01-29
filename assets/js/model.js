ae028fa213349cc99e152f68f6dd0455aa92af92
RoMcGill <ro.mcgill12@gmail.com>
2022-01-29 13:45:23 +0000

    add content/populate model.js

--- assets/js/model.js
+++ assets/js/model.js
@@ -24,6 +24,139 @@ export const tipsData = [
         </p> <p>Try going for three sets of 20 repetitions first. You can also use this quick exercise as a way to warm up before another workout too.
         .</p>`,
       },
+      {
+        id: 3, // uniquely identify this tip
+        used: false, // already displayed tip will be true and used to filter and ensure tips are not repeated
+        detail: `<h2>Seated Leg Raises</h2>
+        <p>With this exercise, you won’t ever have to worry about skipping leg day. You can do these leg and abdominal exercises even when you’re in a meeting or on a conference call without people noticing. And the best part? They target your quadriceps (the front of your thighs), hamstrings (the back of your thighs), and glutes (the butt) — so you get a killer compound exercise without ever getting up. Sit upright in your office chair.</p>
+        <p> Straighten your left leg so that it is parallel to the floor and hold it in place for 10 seconds. Now, do the same thing with your right leg. Repeat both legs for 15 repetitions. Once you build up strength, try adding weight to the exercise by looping your purse or briefcase on your legs while you do the raises.</p>`,
+      },
+      {
+        id: 4, // uniquely identify this tip
+        used: false, // already displayed tip will be true and used to filter and ensure tips are not repeated
+        detail: `<h2>Chair Dips</h2>
+        <p>For this exercise, you need an office chair that won’t roll away from you. Scoot up to the very front edge of your chair, place your legs out in front of you, and place your hands on either side of your hips, fingers pointing towards your desk. Grasp the edges of the chair with both hands, and use your core and arms to raise your body up off the chair and then down so that your rear goes down toward the floor. Push yourself back up,</p>
+        <p>and repeat this 15 times. Do three cycles of 15.</p>`,
+      },
+      {
+        id: 5, // uniquely identify this tip
+        used: false, // already displayed tip will be true and used to filter and ensure tips are not repeated
+        detail: `<h2>Calf Raises</h2>
+        <p>Use water bottles for extra weight or lean on a chair or wall for more stability.
+            Have your hands by your side, lift up to the balls of your feet, go down, lift up again, and repeat this process while keeping your core tight.
+            </p>
+        <p>Stay nice and tall without rounding. Maintain all positions for two or three seconds. Go for two to three sets of to 12 to 15 reps or until you start feeling the burn.
+            </p>`,
+      },
+      {
+        id: 6, // uniquely identify this tip
+        used: false, // already displayed tip will be true and used to filter and ensure tips are not repeated
+        detail: `<h2>Shadow Boxing</h2>
+        <p>Not only will this exercise raise your heart rate and bring in some cardio, it will also be a good stress release if you had a particularly frustrating day at the office. Raise your fists up in front of your face in a boxing position — while you’re sitting a safe distance away from your computer, of course.
+            </p>
+        <p>Punch your fists forward in the air as if you are using a punching bag, switching back and forth from right arm to left. Do this for 30 seconds. Pause. Repeat for 30 seconds. Again, like other cardio exercises, work this repetition into scheduled intervals in your workday.
+            </p>`,
+      },
+      {
+        id: 7, // uniquely identify this tip
+        used: false, // already displayed tip will be true and used to filter and ensure tips are not repeated
+        detail: `<h2>Water Bottle Free Weight</h2>
+        <p>For a simple, effective variant on your favorite free weight exercises, take two filled water bottles and do lateral raises, boxer drills, bicep curls — you name it. Using these as weights, do overhead presses, arm curls, and other simple gym-style workouts right at your desk. For each exercise,
+          </p>
+        <p>try three sets of 10 to 30 repetitions, according to the weight of your water bottles and your current strength. You can even turn these into a full arm day workout.
+          </p>`,
+      },
+      {
+        id: 8, // uniquely identify this tip
+        used: false, // already displayed tip will be true and used to filter and ensure tips are not repeated
+        detail: `<h2>The Leaning Plank</h2>
+        <p>This exercise requires you to get up from your desk chair. But it can be great while you’re waiting in line for the coffee machine or the microwave — or in those few minutes when everyone has left the conference room after a meeting. This is a variant on a plank exercise, using a very similar form.
+          </p>
+        <p> Step back so that you are at least a foot away from a wall and then lean forward against it using only your forearms for support. Hold this position as long as you can. For a variant, get into the leaning position and lower yourself until your shoulders almost touch the wall, too, and push yourself back up. Repeat 15 times.
+          </p>`,
+      },
+      {
+        id: 9, // uniquely identify this tip
+        used: false, // already displayed tip will be true and used to filter and ensure tips are not repeated
+        detail: `<h2>The Chest Opener</h2>
+        <p>This is a quick and effective stretch for anyone who experiences (or wants to avoid) frequent shoulder pain. Sit on the edge of your chair, place both hands behind the lower part of the neck, round this position with your elbows slowly coming in, and hold the chin down. Then, open up your chest and arms while holding your head high and squeezing your shoulder blades, and repeat.
+          </p>
+        <p>Five to 10 repetitions as part of a regular complete workout will do. You can also try doing this stretch several times a day. Make sure you don’t hold your breath so you don’t get tired easily.
+          </p>`,
+      },
+      {
+        id: 10, // uniquely identify this tip
+        used: false, // already displayed tip will be true and used to filter and ensure tips are not repeated
+        detail: `<h2>Lunges</h2>
+        <p>If you want a killer workout for your quads, there aren’t many exercises that beat lunges. Stand with your feet halfway apart at shoulder-width length. Bring one leg to the front, and extend the other towards the back, standing on your toes. This is your starting position. Slowly bend the knee of your front leg, forming a 90-degree angle (the knee shouldn’t extend past the toes), and slowly lunge, while also bending the other leg. Bring your whole body down, until your back knee touches the ground.
+          </p>
+        <p>Do 5 to 15 reps, and then switch legs.You can make this even more challenging by adding some weight. Just get two bottles of water and pretend like you’re holding dumbbells.
+          </p>`,
+      },
+      {
+        id: 11, // uniquely identify this tip
+        used: false, // already displayed tip will be true and used to filter and ensure tips are not repeated
+        detail: `<h2>Desk Push-Ups</h2>
+        <p>This is a great upper body exercise. Place your palms at the edge of your desk (about shoulder-width apart), and move your feet away from the desk until you are inclined towards it. Then, slowly lower your chest down towards the desk while breathing in, and then push yourself back up (as you would during a regular push-up).
+          </p>
+        <p> Repeat as many times as you can. Make sure that you’re wearing the right shoes, or not wearing any at all.
+          </p>`,
+      },
+      {
+        id: 12, // uniquely identify this tip
+        used: false, // already displayed tip will be true and used to filter and ensure tips are not repeated
+        detail: `<h2>Pretend Jump Rope</h2>
+        <p>This is another quick cardio workout that you can sneak into your routine. Stand with your feet close together, extend your forearms in the opposite directions at a low angle, and pretend that you’re holding a jump rope. Make circular motions with your forearms and jump every time they point to the floor. 
+          </p>
+        <p>You don’t necessarily have to do the circular motions, but pretending that you’re holding a jump rope and that you’ll “trip” will help you time your jumps properly, resulting in a smooth workout.
+          </p>`,
+      },
+      {
+        id: 13, // uniquely identify this tip
+        used: false, // already displayed tip will be true and used to filter and ensure tips are not repeated
+        detail: `<h2>The "magic carpet ride"</h2>
+        <p>You don't even need to leave your chair for this move. While sitting on your chair, cross your legs so your feet rest under your ankles. Next, put your hands on the chair's armrests,engage your core, and lift the rest of your body a few inches off the chair.
+          </p>
+        <p>Hold the pose for 10 to 20 seconds, rest, and then repeat for a total of five times.
+          </p>`,
+      },
+      {
+        id: 14, // uniquely identify this tip
+        used: false, // already displayed tip will be true and used to filter and ensure tips are not repeated
+        detail: `<h2>Stair climbing</h2>
+        <p>Assuming you have access to a staircase at your job, here's a simple way to squeeze some cardio into your workday: Set a timer for as many minutes as you have (whether it's two, five, or 10, it'll still be good for you) and take to the stairs.
+          </p>
+        <p>If you're willing to get sweaty, run up and down as fast as you're able. If you're trying to avoid creasing your work outfit, you can still get your heart rate up by walking briskly up and down the stairs. Bonus points if you take them two at a time.
+          </p>`,
+      },
+      {
+        id: 15, // uniquely identify this tip
+        used: false, // already displayed tip will be true and used to filter and ensure tips are not repeated
+        detail: `<h2>Seated Secret Core</h2>
+        <p>Sit tall in your chair with your pelvis slightly tilted forward. Pull your belly button up and in. Remember to brace your core (imagine getting ready for someone to hit your belly).
+          </p>
+        <p> Hold for 10 to 15 seconds, then relax for a breath or two. Repeat five times.
+          </p>`,
+      },
+      {
+        id: 16, // uniquely identify this tip
+        used: false, // already displayed tip will be true and used to filter and ensure tips are not repeated
+        detail: `<h2>Marching Core</h2>
+        <p>Move forward in your seat so that you’re sitting on the edge of it. Engage your core and lean slightly back while keeping your back straight. If that’s challenging enough,
+          </p>
+        <p>hold there for 10 to 15 seconds. You can make this harder by lifting one foot off the ground at a time as well. Relax and repeat five times.
+          </p>`,
+      },
+      {
+        id: 17, // uniquely identify this tip
+        used: false, // already displayed tip will be true and used to filter and ensure tips are not repeated
+        detail: `<h2>Shoulder Blade Pencil Pinches</h2>
+        <p>Think of this as an instant posture improver. Sit up tall, as if you’re balancing a glass of water on your head. Engage your core, then pinch your shoulder blades together (imagine squeezing a pencil between them).
+          </p>
+        <p>Relax and repeat 20 times.
+          </p>`,
+      },
 
+      
 
   ];
